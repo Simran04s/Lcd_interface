41e81cf0b2712bbe4a65251e42d05d7793e85ba9
Simran04s <130791769+Simran04s@users.noreply.github.com>
2025-01-14 14:46:58 +0530

    Add files via upload

---
+++ bhakti/bhakti.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    -int i=3;
+    switch(i){
+        default:print("zero");
+        case1:print("one");
+        break;
+        case 2:print("two");
+        break;
+        case3:print("three");
+        break;
+    }
+    return 0;
+}
